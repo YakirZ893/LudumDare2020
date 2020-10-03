5a614a65e70c0d1929b16205d603ddf46650be36
YakirZ893 <60983683+YakirZ893@users.noreply.github.com>
2020-10-04 02:14:44 +0300

    Created Button Behavior Script
    once Button is pressed ( with button animation) the platform will start to move according to the platform's recorded transform animation XoXo

---
+++ Assets/Scripts/ButtonBehaviour.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonBehaviour : MonoBehaviour
+{
+    public GameObject Platform;
+    public Animator anim;
+
+    private Animator PlatformAnim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlatformAnim = Platform.GetComponent<Animator>();
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+    anim.SetBool("ButtonPressed", true);
+    PlatformAnim.SetBool("Moving", true);
+
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        anim.SetBool("ButtonPressed", false);
+        //PlatformAnim.SetBool("Moving", false); // returns to first animation keyframe/initial position once button is unpressed
+
+    }
+
+
+}
