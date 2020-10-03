1686a91be86dc9ec700754b1288a8bd44414a3d3
ItayKlainman <61244912+ItayKlainman@users.noreply.github.com>
2020-10-03 04:07:16 +0300

    THICC

---
+++ Assets/PlayerInteraction.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteraction : MonoBehaviour
+{
+    SphereCollider SC;
+    void Start()
+    {
+        SC = GetComponent<SphereCollider>();
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        GameObject go = other.gameObject;
+        if (other.gameObject.tag == "CanBeGrabbed" && Input.GetKeyDown(KeyCode.E))
+        {
+            
+            go.transform.parent = this.transform;
+        }
+
+        if (Input.GetKeyUp(KeyCode.E) && go.transform.IsChildOf(this.transform))
+        {
+            go.transform.parent = null;
+            
+        }
+    }
+  
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.Animations;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] float speed;
+    [SerializeField] float jumpforce;
+    [SerializeField] Rigidbody rb;
+   [SerializeField] bool isgrounded;
+
+    private void Start()
+    {
+        rb = this.GetComponent<Rigidbody>();
+        isgrounded = true;
+    }
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        if (x > 0 || x < 0)
+            rb.AddForce(-Vector3.right * x * speed * Time.deltaTime);
+        if (Input.GetKeyDown(KeyCode.Space) && isgrounded)
+        {
+            rb.AddForce(Vector3.up * jumpforce * Time.deltaTime, ForceMode.Impulse);
+            isgrounded = false;    
+        }
+        resetJumps();
+    }
+    private void resetJumps()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position,-Vector3.up, out hit, 1f))
+            if (hit.transform.gameObject.tag == ("Ground"))
+                isgrounded = true;
+        Debug.DrawRay(transform.position, transform.TransformDirection(-Vector3.up) * hit.distance, Color.yellow);
+    }
+}
