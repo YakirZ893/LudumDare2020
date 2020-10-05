577d1536933ee0b5c5b1916f5e1533eb1c78d4fa
ItayKlainman <61244912+ItayKlainman@users.noreply.github.com>
2020-10-05 18:00:30 +0300

    safd;ljk

---
+++ Assets/ResetLevel.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ResetLevel : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player" || other.gameObject.tag == "Recording")
+        {
+            int scene = SceneManager.GetActiveScene().buildIndex;
+            SceneManager.LoadScene(scene);
+        }
+    }
+}
