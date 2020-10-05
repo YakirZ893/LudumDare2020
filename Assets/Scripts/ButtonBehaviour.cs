b85d58b05870571e2b75ba646891fd3ddb92e7c6
Alon Braha <alonbra@post.bgu.ac.il>
2020-10-05 23:27:22 +0300

    Pre build push

--- Assets/Scripts/ButtonBehaviour.cs
+++ Assets/Scripts/ButtonBehaviour.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.Remoting.Messaging;
 using UnityEditor.Build.Reporting;
@@ -12,13 +13,18 @@ public class ButtonBehaviour : MonoBehaviour
     public bool isbuttonclicked;
     public bool isbuttonclickedcannon;
 
-
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player" || other.gameObject.tag == "Recording")
+        {
+            GameObject.FindObjectOfType<AudioManager>().Play("Button");
+        }
+    }
 
     private void OnTriggerStay(Collider other)
     {
         if(other.gameObject.tag == "Player" || other.gameObject.tag =="Recording")
         anim.SetBool("ButtonPressed", true);
-        GameObject.FindObjectOfType<AudioManager>().Play("Button");
         isbuttonclicked = true;
         isbuttonclickedcannon = true;
         
