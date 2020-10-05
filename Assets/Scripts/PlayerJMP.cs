15b670aad68fa38d55a66fbc6f68038fdc3a87e6
ItayKlainman <61244912+ItayKlainman@users.noreply.github.com>
2020-10-05 14:36:27 +0300

    fssadf

--- Assets/Scripts/PlayerJMP.cs
+++ Assets/Scripts/PlayerJMP.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEditor;
 
 [RequireComponent(typeof(Rigidbody))]
 public class PlayerJMP : MonoBehaviour
@@ -7,6 +8,9 @@ public class PlayerJMP : MonoBehaviour
     public Vector3 jump;
     public float jumpForce = 2.0f;
     public bool isGrounded = true;
+    private bool haslanded = false;
+    public Transform smoke;
+    public GameObject smokePrefab;
     Rigidbody rb;
     void Start()
     {
@@ -14,16 +18,17 @@ public class PlayerJMP : MonoBehaviour
         jump = new Vector3(0.0f, 2.0f, 0.0f);
     }
 
-   
-
     void FixedUpdate()
     {
         if (isGrounded && Input.GetKey(KeyCode.Space))
-        {
-            
-            
+        {  
+
             rb.AddForce(jump * jumpForce, ForceMode.Impulse);
+           GameObject go= Instantiate(smokePrefab, smoke.position, Quaternion.Euler(90,0,0));
             isGrounded = false;
+            haslanded = true;
+
+            Destroy(go, 1f);
         
         }
         
@@ -34,7 +39,12 @@ public class PlayerJMP : MonoBehaviour
         if (collision.gameObject.tag == "Ground" || collision.gameObject.tag == "CanBeGrabbed")
         {
             isGrounded = true;
-
+            if (haslanded)
+            {
+                GameObject go = Instantiate(smokePrefab, smoke.position, Quaternion.Euler(90, 0, 0));
+                Destroy(go, 1f);
+                haslanded = false;
+            }
         }
     }
 }
